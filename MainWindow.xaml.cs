9422e2018f2b25d420ee4578b61594c2ff20a4c3
Matthew Franks <42283335+mafranks@users.noreply.github.com>
2021-05-04 09:16:02 -0400

    Fixed XML Parsing issue for multiple results by adding a depth field to the call.  Cleaned up the code a little.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
@@ -19,9 +8,6 @@ using System.Xml;
 
 namespace CiscoSecureEndpointResourceMonitor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -31,7 +17,10 @@ namespace CiscoSecureEndpointResourceMonitor
             // Pull all subdirectories from the AMP directory and parse for version numbers
             var directories = Directory.GetDirectories(path);
             var current_version = showMatch(directories, @"\d{1,2}\.\d{1,2}\.\d{1,2}");
-
+            pullXMLFileInfo(path, current_version);
+        }
+        public void pullXMLFileInfo(string path, string current_version)
+        {
             //Create lists for XML traversal
             List<string> build_list = new List<string> { "agent", "revision" };
             List<string> policy_uuid_list = new List<string>{ "policy", "uuid"};
@@ -46,55 +35,37 @@ namespace CiscoSecureEndpointResourceMonitor
             global_xml.Load($"{path}/{current_version}/global.xml");
             XmlDocument local_xml = new XmlDocument();
             local_xml.Load($"{path}/local.xml");
-            string build = parseXML(global_xml, build_list);
-            string policy_uuid = parseXML(policy_xml, policy_uuid_list);
-            string policy_name = parseXML(policy_xml, policy_name_list);
-            string policy_serial = parseXML(policy_xml, policy_serial_list);
-            // TETRA has two entries.  Need to figure a way around this.
-//            string tetra_version = parseXML(local_xml, tetra_version_list);
-//            Trace.WriteLine(tetra_version);
+            string build = parseXML(global_xml, build_list, 0);
+            string policy_uuid = parseXML(policy_xml, policy_uuid_list, 0);
+            string policy_name = parseXML(policy_xml, policy_name_list, 0);
+            string policy_serial = parseXML(policy_xml, policy_serial_list, 0);
+            string tetra_version = parseXML(local_xml, tetra_version_list, 1);
 
             // Populate the GUI
             VersionText.Text = $"{current_version}.{build}";
             PolicyNameText.Text = policy_name;
             PolicyUUIDText.Text = policy_uuid;
             PolicySerialText.Text = policy_serial;
-//            TETRAVersionText.Text = tetra_version;
+            TETRAVersionText.Text = $"{tetra_version.Split(':')[1]}";
         }
-        private string parseXML(XmlDocument xml, List<string> list)
+        public string parseXML(XmlDocument xml, List<string> list, int depth)
         {
             XmlNodeList XMLNodeList = xml.GetElementsByTagName(list[0]);
-            string item1 = XMLNodeList[0][list[1]].InnerText;
+            string item1 = XMLNodeList[depth][list[1]].InnerText;
             return item1;
-
-            /*** DO NOT DELETE UNTIL YOU HAVE A VALID REPLACEMENT!!
-            // Create XML from file and parse based on required path
-            XmlDocument xml = new XmlDocument();
-            xml.Load(path);
-            XmlNodeList XMLNodeList = xml.GetElementsByTagName("business");
-            string bus_uuid = XMLNodeList[0]["uuid"].InnerText;
-            XmlNodeList XMLNodeList2 = xml.GetElementsByTagName("policy");
-            string policy_uuid = XMLNodeList2[0]["uuid"].InnerText;
-            string policy_serial = XMLNodeList2[0]["serial_number"].InnerText;
-            string policy_name = XMLNodeList2[0]["name"].InnerText;
-            ***/
         }
-
-
-            private void StartButton_Click(object sender, RoutedEventArgs e)
+        public void StartButton_Click(object sender, RoutedEventArgs e)
         {
             // Enabled/Disable button if the other is pressed
             StopButton.IsEnabled = true;
             StartButton.IsEnabled = false;
         }
-
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
             // Enabled/Disable button if the other is pressed
             StartButton.IsEnabled = true;
             StopButton.IsEnabled = false;
         }
-
         public string showMatch(string[] directories, string expr)
         {
             // Walk through each regex response for version and replace if it is higher
