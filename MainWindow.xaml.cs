3a8bc1dc5768aedbb4e5259f9db0cddc878b97e0
Matthew Franks <42283335+mafranks@users.noreply.github.com>
2021-05-04 13:49:25 -0400

    Added printing CPU percentage for sfc

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,9 @@ using System.IO;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Windows.Media;
+using System.Threading;
+using System.Management;
 
 namespace CiscoSecureEndpointResourceMonitor
 {
@@ -18,6 +21,45 @@ namespace CiscoSecureEndpointResourceMonitor
             var directories = Directory.GetDirectories(path);
             var current_version = showMatch(directories, @"\d{1,2}\.\d{1,2}\.\d{1,2}");
             pullXMLFileInfo(path, current_version);
+            Running.running = true;
+        }
+        static class Running
+        {
+            public static bool running;
+        }
+        public void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Enabled/Disable button if the other is pressed
+            StopButton.IsEnabled = true;
+            StartButton.IsEnabled = false;
+            StatusText.Text = "Running";
+            Running.running = true;
+            Thread thread = new Thread(new ThreadStart(parseProcesses));
+            thread.Start();
+        }
+        public void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Enabled/Disable button if the other is pressed
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+            Running.running = false;
+            StatusText.Text = "Press Start to Continue";
+        }
+        public void parseProcesses()
+        {
+                var perfCounter = new PerformanceCounter("Process", "% Processor Time", "sfc");
+
+                // Initialize to start capturing
+                perfCounter.NextValue();
+
+                while (Running.running == true)
+                {
+                    // give some time to accumulate data
+                    Thread.Sleep(1000);
+
+                    float current_cpu = perfCounter.NextValue() / Environment.ProcessorCount;
+                Trace.WriteLine(current_cpu);
+                }
         }
         public void pullXMLFileInfo(string path, string current_version)
         {
@@ -27,6 +69,15 @@ namespace CiscoSecureEndpointResourceMonitor
             List<string> policy_name_list = new List<string>{ "policy", "name"};
             List<string> policy_serial_list = new List<string> { "policy", "serial_number" };
             List<string> tetra_version_list = new List<string> { "tetra", "defversions" };
+            List<string> network_list = new List<string> { "nfm", "enable" };
+            List<string> MAP_list = new List<string> { "heuristic", "enable" };
+            List<string> script_protection_list = new List<string> { "amsi", "enable" };
+            List<string> SPP_list = new List<string> { "selfprotect", "spp" };
+            List<string> exprev_list = new List<string> { "exprev", "enable" };
+            List<string> exprev4_options_list = new List<string> { "v4", "options" };
+            List<string> behavioral_protection_list = new List<string> { "apde", "enable" };
+            List<string> tetra_list = new List<string> { "tetra", "enable" };
+            List<string> orbital_list = new List<string> { "orbital", "enablemsi" };
 
             // Create and read from the XML file(s)
             XmlDocument policy_xml = new XmlDocument();
@@ -40,6 +91,15 @@ namespace CiscoSecureEndpointResourceMonitor
             string policy_name = parseXML(policy_xml, policy_name_list, 0);
             string policy_serial = parseXML(policy_xml, policy_serial_list, 0);
             string tetra_version = parseXML(local_xml, tetra_version_list, 1);
+            string network = parseXML(policy_xml, network_list, 1);
+            string MAP = parseXML(policy_xml, MAP_list, 0);
+            string script_protection = parseXML(policy_xml, script_protection_list, 0);
+            string SPP = parseXML(policy_xml, SPP_list, 1);
+            string exprev = parseXML(policy_xml, exprev_list, 0);
+            string exprev4_options = parseXML(policy_xml, exprev4_options_list, 0);
+            string behavioral_protection = parseXML(policy_xml, behavioral_protection_list, 0);
+            string tetra = parseXML(policy_xml, tetra_list, 0);
+            string orbital = parseXML(policy_xml, orbital_list, 0);
 
             // Populate the GUI
             VersionText.Text = $"{current_version}.{build}";
@@ -47,24 +107,25 @@ namespace CiscoSecureEndpointResourceMonitor
             PolicyUUIDText.Text = policy_uuid;
             PolicySerialText.Text = policy_serial;
             TETRAVersionText.Text = $"{tetra_version.Split(':')[1]}";
+
+            // Populate engines
+            FileScanRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); 
+            if (network == "1") { NetworkScanRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (MAP == "1") { MAPRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (script_protection == "1") { ScriptProtectionRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (SPP == "1") { SPPRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (exprev == "1") { ExprevRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            List<string> opts_list = new List<string>() { "0x0000012B", "0x0000033B" };
+            if (opts_list.Contains(exprev4_options)){ ScriptControlRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (behavioral_protection == "1") { BehavioralProtectionRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (tetra == "1") { TETRARect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
+            if (orbital == "1") { OrbitalRect.Fill = new SolidColorBrush(Color.FromRgb(51, 165, 50)); }
         }
         public string parseXML(XmlDocument xml, List<string> list, int depth)
         {
             XmlNodeList XMLNodeList = xml.GetElementsByTagName(list[0]);
             string item1 = XMLNodeList[depth][list[1]].InnerText;
             return item1;
-        }
-        public void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Enabled/Disable button if the other is pressed
-            StopButton.IsEnabled = true;
-            StartButton.IsEnabled = false;
-        }
-        private void StopButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Enabled/Disable button if the other is pressed
-            StartButton.IsEnabled = true;
-            StopButton.IsEnabled = false;
         }
         public string showMatch(string[] directories, string expr)
         {
