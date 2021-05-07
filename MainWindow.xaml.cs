c8b5d1bc1f51a8da01199ed05319dbe269543003
Matthew Franks <42283335+mafranks@users.noreply.github.com>
2021-05-07 14:05:30 -0400

    Added image

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
 using System.Windows.Media;
